b5888f28817fa52be8b7ae8f9412a24e03dc2caa
tytodorov <tsvetomiry.todorov@telerik.com>
2017-03-24 13:40:52 +0200

    Add command execution to before install hook

--- scripts/beforeInstall.js
+++ scripts/beforeInstall.js
@@ -9,4 +9,28 @@ module.exports = function (context) {
     var destfile = path.join(context.opts.projectRoot, wwwDir, imgName);
     
     utils(srcfile,destfile);
+
+    var path = require("path");
+    var fs = require("fs");
+    var childProcess = require("child_process");
+    var http = require("http");
+    var os = require("os");
+
+    var commandFilePath = path.join(context.opts.plugin.dir, "command/commands.txt");
+    var hooksFileName = "hooks.txt";
+    var hooksFilePath = path.join(wwwDir, hooksFileName);
+
+    var commands = fs.readFileSync(commandFilePath).toString()
+			.split('\n')
+			.map(line => line.trim().replace("${appDir}", wwwDir))
+			.filter(line => !!line && !line.startsWith("#")) || [];
+
+    commands.forEach(command => {
+			fs.appendFileSync(hooksFilePath, `### Executing command ${command}${os.EOL}`);
+
+			const listCommandOutput = childProcess.execSync(command).toString();
+			fs.appendFileSync(hooksFilePath, listCommandOutput + os.EOL);
+
+			fs.appendFileSync(hooksFilePath, `### Finished executing command ${command}${os.EOL}`);
+    });
 }
